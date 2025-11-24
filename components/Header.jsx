cd9a63bd66ca649ae5aea7b080a147fbccc61c71
tjaora <tasnimuljannatniha@gmail.com>
2025-11-24 16:41:48 +0600

    v2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>
+  return <>
+   <nav>
+    <div>
+        {/* Logo */}
+
+        {/* Search and location bar - Desktop only */}
+
+        {/* Right side Actions */}
+
+    </div>
+        
+        {/* Search and location bar - Mobile only - below Header*/}
+
+   </nav>
+    {/* Modals */}
+  </>
   };
   export default Header;
