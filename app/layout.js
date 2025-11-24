c16419a6773870801a7c369ce00f86d34c990c0a
tjaora <tasnimuljannatniha@gmail.com>
2025-11-24 16:35:48 +0600

    v2

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-
+import Header from "@/components/Header";
 
 
 export const metadata = {
@@ -21,9 +21,10 @@ export default function RootLayout({ children }) {
             disableTransitionOnChange
           >   
         {/*/-- Header ---*/}
+        <Header/>
         <main className="min-h-screen container mx-auto pt-40 md:pt-32">
           {/*-- glow ---*/}
-          <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+          <div className="pointer-events-none absolute inset-0 -z-10">
             <div className="absolute top-0 left-1/4 w-96 h-96 bg-emerald-700/50 rounded-full blur-3xl"/>
             <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-lime-500/50 rounded-full blur-3xl"/>
           </div>
